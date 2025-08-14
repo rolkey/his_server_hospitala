88e45f594833ccce0d15f6924daea134c5f295fb
linxin <1084848355@qq.com>
2025-08-14 19:06:36 +0800

    新增 h11_zybh 模块及相关服务和控制器，添加获取当前住院编号的接口

--- src/modules/h11_zybh/h11_zybh.controller.ts
+++ src/modules/h11_zybh/h11_zybh.controller.ts
@@ -5,9 +5,9 @@ import { h11_zybhService } from './h11_zybh.service';
 export class h11_zybhController {
   constructor(private readonly h11_zybhService: h11_zybhService) {}
 
-  @Get('findZY')
-  async findOne() {
-    const results = await this.h11_zybhService.findOne();
+  @Get('findCurrentZYBH')
+  async findCurrentZYBH() {
+    const results = await this.h11_zybhService.findCurrentZYBH();
     return results;
   }
 }
