c2b192c0bbc6002a06adcb9fdbd47c880b38cd40
linxin <1084848355@qq.com>
2025-12-01 15:50:14 +0800

    添加强制删除功能及相关DTO和服务逻辑

--- src/modules/h11_lsh/h11_lsh.module.ts
+++ src/modules/h11_lsh/h11_lsh.module.ts
@@ -8,10 +8,13 @@ import { h11_brxxService } from '../h11_brxx/h11_brxx.service';
 import { h11_lshController } from './h11_lsh.controller';
 import { h00_fylbService } from '../h00_fylb/h00_fylb.service';
 import { h00_fylb } from '../h00_fylb/h00_fylb.entity';
+import { Syspar } from '../h12_xmzd/entity/syspar.entity';
+import { SysparNew } from '../h12_xmzd/entity/__syspar_new.entity';
+import { ParamService } from '../h12_xmzd/service/param.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([h11_lsh, h11_brxx, H11Jszb, h00_fylb])],
-  providers: [h11_lshService, h11_brxxService, h00_fylbService],
+  imports: [TypeOrmModule.forFeature([h11_lsh, h11_brxx, H11Jszb, h00_fylb, Syspar, SysparNew])],
+  providers: [h11_lshService, h11_brxxService, h00_fylbService, ParamService],
   controllers: [h11_lshController], // 确保h11_lshController被导入
   exports: [h11_lshService],
 })
