94b7a58df8911e607f541f24cca224e2d1b674ae
马海军 <rolkey@gmail.com>
2026-01-07 10:09:06 +0800

    统一消息

--- src/modules/h12_yzzb/h12_yzxb.service_new.ts
+++ src/modules/h12_yzzb/h12_yzxb.service_new.ts
@@ -415,7 +415,7 @@ export class h12_yzxbServiceNew {
       this.logger.error('执行医嘱失败', error);
       throw new CustomException(
         error.code ?? ERR.ERR_40002.code,
-        error?.message ?? '执行医嘱失败',
+        error?.message ?? ERR.ERR_40002.message,
         null,
         error.data,
       );
