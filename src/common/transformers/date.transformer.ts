668d56f51a481e544454dd5fd40b7a9121dde925
马海军 <rolkey@gmail.com>
2026-01-21 10:37:45 +0800

    避免传入日期不正确时，处理数据不正确

--- src/common/transformers/date.transformer.ts
+++ src/common/transformers/date.transformer.ts
@@ -3,7 +3,12 @@ import DateFormater from '@/utils/DateFormater';
 
 export const DateTransformer = () => {
   return Transform(({ value }) => {
-    if (!value) return value;
+    if (!value) return null;
+
+    const date = new Date(value);
+    if (isNaN(date.getTime())) {
+      return null;
+    }
 
     return DateFormater.formatDate(value);
   });
