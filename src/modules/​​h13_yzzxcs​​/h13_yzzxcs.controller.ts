d94b99a578860a40e3037e045b263a533b85638e
马海军 <rolkey@gmail.com>
2026-01-30 16:15:28 +0800

    还原

--- src/modules/​​h13_yzzxcs​​/h13_yzzxcs.controller.ts
+++ src/modules/​​h13_yzzxcs​​/h13_yzzxcs.controller.ts
@@ -141,13 +141,11 @@ export class h13_yzzxcsController {
 
   // 撤回退费领药单：有领药单没有发药，已经有领药单
   @Put('revoke-refund-medicine-receipt')
-  async revokeRefundMedicineReceipt(
-    @Body() data: { zyid: string; maxidList: Array<{ maxid: number }> },
-  ) {
+  async revokeRefundMedicineReceipt(@Body() data: { zyid: string; maxids: string }) {
     // 将逗号分隔的字符串转换为数字数组
     return await this.h13_yzzxcsService.revokeRefundMedicineReceipt({
       zyid: data.zyid,
-      maxids: data.maxidList.map((item) => item.maxid),
+      maxids: data.maxids.split(',').map(Number),
     });
   }
 }
