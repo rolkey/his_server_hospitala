e7fe2ebfde817966aad568b85d3c2d1420678515
马海军 <rolkey@gmail.com>
2026-02-05 19:52:01 +0800

    加限制

--- src/modules/h12_yzzb/h12_yzxb.service.ts
+++ src/modules/h12_yzzb/h12_yzxb.service.ts
@@ -1478,7 +1478,7 @@ export class h12_yzxbService {
       mrcs: null,
     };
     const yzxbs = await this.h12_yzxbRepo.find({
-      where: { yzlx, yzxh, zyid, yzzh: In(yzzh) },
+      where: { yzlx, yzxh, zyid, yzzh: In(yzzh), yzzt: In([5, 7]) },
     });
     for (const yzxb of yzxbs) {
       Object.assign(yzxb, defaultValue);
