d09dc7929da86727a098ad83404c3c07e16e13e8
马海军 <rolkey@gmail.com>
2026-03-07 12:09:33 +0800

    大小写问题

--- src/modules/permission/permission.module.ts
+++ src/modules/permission/permission.module.ts
@@ -1,15 +1,13 @@
-
-
 import { Module } from '@nestjs/common';
 import { PermissionService } from './permission.service';
 import { PermissionController } from './permission.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Permission } from './permission.entity';
-import { Module as module } from '@/modules/Module/Module.entity';
+import { Module as module } from '@/modules/module/module.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Permission, module])],
   controllers: [PermissionController],
   providers: [PermissionService],
 })
-export class PermissionModule { }
+export class PermissionModule {}
--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './role.entity';
 import { Permission } from '@/modules/permission/permission.entity';
 import { usrcat } from '../usrcat/usrcat.entity';
-import { Module as module } from '@/modules/Module/Module.entity';
+import { Module as module } from '@/modules/module/module.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role, Permission, usrcat, module])],
